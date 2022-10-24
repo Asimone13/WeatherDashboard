347aef5ef9b9bc4be72a03da71ad579c9ba800cc
Alec Simone <alecsimone2@gmail.com>
2022-10-24 10:52:09 -0400

    started js

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+var city="";
+
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
